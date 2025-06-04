a9de21d51a7bd6290eeeeb3b7e1cdb5c6ad5efdd
ezS8ot <dream11203@live.com>
2025-06-04 13:56:26 +0800

    ADD SINCE QUERY ARG

--- server/api/handlers/record.py
+++ server/api/handlers/record.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import logging
 import asyncio
+from typing import Optional
+from datetime import datetime
 import itertools
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, Query, Response
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from server.schemas import RecordList
 from server.deps import get_db, get_openid
@@ -27,10 +29,15 @@ async def save_records(
 
 @router.get(routing_table.GET_RECORD)
 async def get_records(
+    since: Optional[datetime] = Query(None),
     db: AsyncIOMotorDatabase = Depends(get_db),
     openid: str = Depends(get_openid),
 ):
-    cursor = db.records.find({"openid": openid})
-    records = await cursor.to_list(100)
-    return records
+    query = {"openid": openid}
+    if since:
+        query["updatedAt"] = {"$gte": since}
+
+    cursor = db.records.find(query)
+    records = await cursor.to_list(length=100)
+    return {"success": True, "updatedRecords": records}
 
