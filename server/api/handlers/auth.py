faf470a765aecf45608d939eadcf18ad59045fc1
ezS8ot <dream11203@live.com>
2025-06-19 16:19:27 +0800

    FIX HANDLE POST PAYLOAD

--- server/api/handlers/auth.py
+++ server/api/handlers/auth.py
@@ -3,6 +3,7 @@ import logging
 import asyncio
 import itertools
 from fastapi import APIRouter, Response
+from pydantic import BaseModel
 from server.auth import wx_login, create_jwt
 from .. import routing_table
 
@@ -10,9 +11,12 @@ logger = logging.getLogger("api_handler")
 
 router = APIRouter()
 
+class LoginRequest(BaseModel):
+    code: str
+
 @router.post(routing_table.LOGIN)
-async def login(code: str):
-    openid = await wx_login(code)
+async def login(body: LoginRequest):
+    openid = await wx_login(body.code)
     if not openid:
         return {"error": "登录失败"}
     token = create_jwt(openid)
